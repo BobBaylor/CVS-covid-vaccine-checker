0e9e10e51b0de718338d6ea2dc2fb2cd18d7901d
Bob Baylor <SkiBoyBob@gmail.com>
2021-03-28 09:08:36 -0700

    add cli (docopt). lint. requirements. cities.

---
+++ cities.py
@@ -0,0 +1,397 @@
+cities_near = [
+    'ALAMEDA',
+    'ALAMO',
+    'ALBANY',
+    'ANTIOCH',
+    'APTOS',
+    'BERKELEY',
+    'BRENTWOOD',
+    'CAMPBELL',
+    'CAPITOLA',
+    'COLMA',
+    'CONCORD',
+    'CUPERTINO',
+    'DANVILLE',
+    'DUBLIN',
+    'EAST PALO ALTO',
+    'EL CERRITO',
+    'EMERYVILLE',
+    'FREMONT',
+    'GILROY',
+    'HALF MOON BAY',
+    'HAYWARD',
+    'LAFAYETTE',
+    'LIVERMORE',
+    'LOS GATOS',
+    'MENLO PARK',
+    'MILL VALLEY',
+    'MORAGA',
+    'MOUNTAIN VIEW',
+    'NEWARK',
+    'OAKLAND',
+    'ORINDA',
+    'PALO ALTO',
+    'PINOLE',
+    'PITTSBURG',
+    'PLEASANTON',
+    'REDWOOD CITY',
+    'RICHMOND',
+    'SAN ANSELMO',
+    'SAN BRUNO',
+    'SAN CARLOS',
+    'SAN FRANCISCO',
+    'SAN JOSE',
+    'SAN LEANDRO',
+    'SAN MATEO',
+    'SAN RAFAEL',
+    'SAN RAMON',
+    'SANTA CLARA',
+    'SAUSALITO',
+    'SCOTTS VALLEY',
+    'SUNNYVALE',
+    'TRACY',
+    'WALNUT CREEK',
+    ]
+
+cities_scc = [
+    'CAMPBELL',
+    'CUPERTINO',
+    'EAST PALO ALTO',
+    'GILROY',
+    'LOS GATOS',
+    'MENLO PARK',
+    'MOUNTAIN VIEW',
+    'PALO ALTO',
+    'SAN JOSE',
+    'SANTA CLARA',
+    'SUNNYVALE',
+    ]
+
+"""
+City/Town	Status
+Adelanto
+Agoura Hills
+Alameda
+Alamo
+Albany
+Alhambra
+Aliso Viejo
+Alpine
+Anaheim
+Anderson
+Angels Camp
+Antioch
+Apple Valley
+Aptos
+Arcadia
+Arcata
+Arleta
+Arroyo Grande
+Artesia
+Arvin
+Atwater
+Auburn
+Azusa
+Bakersfield
+Baldwin Park
+Bell
+Berkeley
+Brea
+Brentwood
+Buellton
+Buena Park
+Burbank
+Camarillo
+Cameron Park
+Campbell
+Canoga Park
+Capitola
+Carlsbad
+Carmel
+Carmichael
+Carpinteria
+Carson
+Cathedral City
+Cerritos
+Chico
+Chino
+Chino Hills
+Chula Vista
+Citrus Heights
+City Of Industry
+Clovis
+Coachella
+Colma
+Commerce
+Compton
+Concord
+Corona
+Costa Mesa
+Covina
+Culver City
+Cupertino
+Cypress
+Daly City
+Dana Point
+Danville
+Davis
+Diamond Bar
+Dixon
+Downey
+Duarte
+Dublin
+Eagle Rock
+East Palo Alto
+Eastvale
+El Cajon
+El Centro
+El Cerrito
+El Monte
+Elk Grove
+Emeryville
+Encinitas
+Encino
+Escondido
+Eureka
+Fairfield
+Fallbrook
+Fillmore
+Folsom
+Fontana
+Foothill Ranch
+Fountain Valley
+Fowler
+Fremont
+Fresno
+Fullerton
+Garden Grove
+Gardena
+Gilroy
+Glendale
+Goleta
+Granada Hills
+Granite Bay
+Greenbrae
+Gridley
+Half Moon Bay
+Hanford
+Hawaiian Gardens
+Hawthorne
+Hayward
+Healdsburg
+Hemet
+Hermosa Beach
+Hesperia
+Highland
+Hollywood
+Huntington Beach
+Huntington Park
+Indian Wells
+Indio
+Inglewood
+Irvine
+Jackson
+Kingsburg
+La Canada Flintridge
+La Habra
+La Jolla
+La Mesa
+La Mirada
+La Puente
+La Quinta
+La Verne
+Ladera Ranch
+Lafayette
+Laguna Niguel
+Laguna Woods
+Lake Elsinore
+Lake Forest
+Lakewood
+Lamont
+Lancaster
+Lathrop
+Lawndale
+Lincoln
+Livermore
+Lodi
+Lomita
+Lompoc
+Long Beach
+Los Alamitos
+Los Angeles
+Los Gatos
+Madera
+Malibu
+Manhattan Beach
+Manteca
+Mar Vista
+Marina
+Martinez
+Marysville
+Mckinleyville
+Menifee
+Menlo Park
+Merced
+Mill Valley
+Mission Hills
+Mission Viejo
+Modesto
+Monrovia
+Montclair
+Montebello
+Monterey
+Monterey Park
+Moorpark
+Moraga
+Moreno Valley
+Mount Shasta
+Mountain View
+Murrieta
+Napa
+National City
+Newark
+Newbury Park
+Norco
+North Hollywood
+Northridge
+Norwalk
+Novato
+Oakhurst
+Oakland
+Oceanside
+Ontario
+Orange
+Orangevale
+Orinda
+Oroville
+Oxnard
+Palm Desert
+Palm Springs
+Palmdale
+Palo Alto
+Paramount
+Pasadena
+Paso Robles
+Patterson
+Petaluma
+Pico Rivera
+Pinole
+Pittsburg
+Placentia
+Placerville
+Playa Vista
+Pleasant Hill
+Pleasanton
+Pomona
+Porterville
+Poway
+Prather
+Ramona
+Rancho Bernardo
+Rancho Cucamonga
+Rancho Santa Margarita
+Red Bluff
+Redding
+Redlands
+Redondo Beach
+Redwood City
+Reedley
+Rialto
+Richmond
+Ripon
+Riverbank
+Riverside
+Rocklin
+Rohnert Park
+Rolling Hills Estates
+Roseville
+Sacramento
+Salinas
+San Anselmo
+San Bernardino
+San Bruno
+San Carlos
+San Clemente
+San Diego
+San Dimas
+San Francisco
+San Gabriel
+San Jacinto
+San Jose
+San Leandro
+San Luis Obispo
+San Marcos
+San Mateo
+San Pedro
+San Rafael
+San Ramon
+Sand City
+Sanger
+Santa Ana
+Santa Barbara
+Santa Clara
+Santa Clarita
+Santa Cruz
+Santa Maria
+Santa Monica
+Santa Paula
+Santa Rosa
+Santee
+Sausalito
+Scotts Valley
+Seal Beach
+Sebastopol
+Signal Hill
+Simi Valley
+Solana Beach
+Soledad
+Solvang
+Sonoma
+Sonora
+South Gate
+South Lake Tahoe
+Spring Valley
+Stanton
+Stockton
+Studio City
+Sun Valley
+Sunnyvale
+Sylmar
+Tahoe City
+Tarzana
+Temecula
+Temple City
+Thousand Oaks
+Torrance
+Tracy
+Tujunga
+Tulare
+Turlock
+Tustin
+Ukiah
+Upland
+Vacaville
+Valencia
+Vallejo
+Van Nuys
+Venice
+Ventura
+Victorville
+Visalia
+Vista
+Walnut Creek
+Walnut Park
+Watsonville
+West Covina
+West Hills
+West Hollywood
+Westlake Village
+Westminster
+Whittier
+Wilmington
+Winchester
+Woodland
+Woodland Hills
+Yorba Linda
+Yuba City
+"""
--- vaccine.py
+++ vaccine.py
@@ -1,50 +1,93 @@
-'''
+"""
+as seen in
+https://python.plainenglish.io/how-i-built-a-cvs-vaccine-appointment-availability-checker-in-python-6beb379549e4
+
 This is a python script that requires you have python installed, or in a cloud environment.
 
 This script scrapes the CVS website looking for vaccine appointments in the cities you list.
-To update for your area, update the locations marked with ### below.
+To update for your area, update the locations marked with ### below and the cities.py list.
 
 If you receive an error that says something is not install, type
 
 pip install beepy
+and
+pip install docopt
 
 in your terminal.
-'''
 
+todo: walgrens, safeway, rite aid, kaiser, blue shield
+"""
 
-
-import requests
+# ignore too-long lines                 --  pylint: disable=C0301
 import time
-import beepy
+import requests
+from beepy import beep
+from docopt import docopt
+
+import cities
 
 
-def findAVaccine():
-    hours_to_run = 3 ###Update this to set the number of hours you want the script to run.
+def find_a_vaccine(opts, city_lst):
+    """ loop for --time hours, and every --period seconds:
+            get the CVS vaccine availability
+            if found, display the CVS city and make a sound
+            else, print a '.' (period)
+    """
+    hours_to_run = int(opts['--time']) # command line argument
     max_time = time.time() + hours_to_run*60*60
     while time.time() < max_time:
 
-        state = 'IL' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
+        state = 'CA' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
 
-        response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+        response = requests.get(
+            "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()),
+            headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
         payload = response.json()
 
         mappings = {}
         for item in payload["responsePayloadData"]["data"][state]:
             mappings[item.get('city')] = item.get('status')
 
-        print(time.ctime())
-        cities = ['BELLEVILLE', 'CHICAGO', 'DEKALB', 'WAUKEGAN'] ###Update with your cities nearby
-        for city in cities:
-            print(city, mappings[city])
+        got_lst = []
+        for key in mappings:
+            if (key in city_lst) and (mappings[key] != 'Fully Booked'):
+                got_lst += [f'{key:30s} - {mappings[key]}']
+        if got_lst:
+            print('\n', time.ctime(), 'success!')
+            print('\n'.join(got_lst))
+            beep(sound='success')
+        else:
+            print('.', end='', flush=True)
+        time.sleep(int(opts['--period'])) # command line argument
 
-        for key in mappings.keys():
-            if (key in cities) and (mappings[key] != 'Fully Booked'):
-                beepy.beep(sound = 'coin')
-                break
-            else:
-                pass
 
-        time.sleep(60) ##This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
-        print('\n')
 
-findAVaccine() ###this final line runs the function. Your terminal will output the cities every 60seconds
+USAGE_STR = """
+Usage:
+  vaccine  [--bay] [--debug=<D>] [--period=<P>] [--time=<T>] [--help]
+  vaccine -h | --help
+
+Options:
+  -b --bay          Greater SF Bay Area; else only Santa Clara County
+  -d --debug=<D>    Bitfield: 1 for cities w/ stock  [default: 0]
+  -h --help         Show this help screen.
+  -p --period=<P>   Seconds between scrapes [default: 60]
+  -t --time=<T>     Hours to loop [default: 3]
+  """
+
+
+if __name__ == '__main__':
+    CLI_OPTS = docopt(USAGE_STR)
+    if CLI_OPTS['--bay']:                   # command line argument
+        CITIES = cities.cities_near     # cities within a 2 hour drive of San Jose, CA
+    else:
+        CITIES = cities.cities_scc      # cities in Santa Cara County
+
+    if int(CLI_OPTS['--debug']) & 1:        # add a couple of cities that probably have stock
+        CITIES += ['BAKERSFIELD', 'FRESNO',]
+    MY_CITY_STR = ', '.join('%s%14s'%('' if i%6 != 0 else '\n', x) for i, x in enumerate(CITIES))
+    print('\n',
+          time.ctime(),
+          f'checking every {CLI_OPTS["--period"]} seconds for {CLI_OPTS["--time"]} hours',
+          MY_CITY_STR)
+    find_a_vaccine(CLI_OPTS, CITIES)
