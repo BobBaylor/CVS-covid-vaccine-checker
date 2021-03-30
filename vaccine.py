872fc3bc0609f9abd59a1fce6297abd2a959159c
Bob Baylor <SkiBoyBob@gmail.com>
2021-03-30 08:52:11 -0700

    start checking the county. slight refactor.

--- vaccine.py
+++ vaccine.py
@@ -27,43 +27,59 @@ from docopt import docopt
 import cities
 
 
-def find_a_vaccine(opts, city_lst):
-    """ loop for --time hours, and every --period seconds:
-            get the CVS vaccine availability
-            if found, display the CVS city and make a sound
-            else, print a '.' (period)
+def check_scc(opts):
+    """see if the Santa Clara County site is mmaking appointments
     """
-    city_str_len = max([len(x) for x in city_lst])
-    hours_to_run = int(opts['--time']) # command line argument
-    max_time = time.time() + hours_to_run*60*60
-    while time.time() < max_time:
-
-        state = 'CA' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
-
-        response = requests.get(
-            "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()),
-            headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
-        payload = response.json()
-
-        mappings = {}
-        for item in payload["responsePayloadData"]["data"][state]:
-            mappings[item.get('city')] = item.get('status')
-
-        got_lst = []
-        ct_str = time.ctime()
-        for key in mappings:
-            if (key in city_lst) and (mappings[key] != 'Fully Booked'):
-                got_lst += [f'{ct_str} - {key:{city_str_len}s} - {mappings[key]}']
-        if got_lst:
-            print('\n'.join(got_lst))
+    response = requests.get(
+        'https://vax.sccgov.org/',
+        headers={"Referer":r'https://www.sccgov.org/sites/covid19/Pages/COVID19-vaccine-information-for-public.aspx#appointment'})
+    ct_str = time.ctime()
+    try:
+        landing = response.links['canonical']['url']
+        if not 'no_appointments' in landing:
+            print('\n', ct_str, landing)
             with open(opts['--file'], 'a') as f_out:
-                f_out.write('\n'.join(got_lst))
-                f_out.write('\n')
+                f_out.write(f'{ct_str} {landing}\n')
             if not opts['--silent']:
                 beep(sound='success')
-        else:
-            print('.', end='', flush=True)
-        time.sleep(int(opts['--period'])) # command line argument
+    # if links missing eaither canonical or url,
+    except IndexError:
+        print('\n', ct_str, 'missing canonical url links:', response.links)
+        beep(sound='error')
+
+
+def find_cvs_vaccine(opts, city_lst):
+    """ get the CVS vaccine availability
+        if found, display the CVS city and make a sound
+        else, print a '.' (period)
+    """
+    city_str_len = max([len(x) for x in city_lst])
+
+    state = 'CA' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
+
+    response = requests.get(
+        "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()),
+        headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+    payload = response.json()
+
+    mappings = {}
+    for item in payload["responsePayloadData"]["data"][state]:
+        mappings[item.get('city')] = item.get('status')
+
+    got_lst = []
+    ct_str = time.ctime()
+    for key in mappings:
+        if (key in city_lst) and (mappings[key] != 'Fully Booked'):
+            got_lst += [f'{ct_str} - {key:{city_str_len}s} - {mappings[key]}']
+    if got_lst:
+        print('\n'.join(got_lst))
+        with open(opts['--file'], 'a') as f_out:
+            f_out.write('\n'.join(got_lst))
+            f_out.write('\n')
+        if not opts['--silent']:
+            beep(sound='success')
+    else:
+        print('.', end='', flush=True)
 
 
 
@@ -97,4 +113,11 @@ if __name__ == '__main__':
           time.ctime(),
           f'checking every {CLI_OPTS["--period"]} seconds for {CLI_OPTS["--time"]} hours',
           MY_CITY_STR)
-    find_a_vaccine(CLI_OPTS, CITIES)
+
+    # loop every --period seconds for --time hours
+    HOURS_TO_RUN = int(CLI_OPTS['--time']) # command line argument
+    MAX_TIME = time.time() + HOURS_TO_RUN*60*60
+    while time.time() < MAX_TIME:
+        find_cvs_vaccine(CLI_OPTS, CITIES)
+        check_scc(CLI_OPTS)
+        time.sleep(int(CLI_OPTS['--period'])) # command line argument
